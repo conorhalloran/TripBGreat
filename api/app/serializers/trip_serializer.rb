d5f1051ea12ae28857202d550b9d411c194aa452
conorhalloran <conorhalloran@gmail.com>
2017-11-29 11:21:55 -0800

    Update Trips Controller and Serializer to Transmit Desired Data

--- api/app/serializers/trip_serializer.rb
+++ api/app/serializers/trip_serializer.rb
@@ -1,4 +1,21 @@
 class TripSerializer < ActiveModel::Serializer
-  attributes :id, :title, :description, :location, :start_date, :end_date, :duration, :aasm_state
-  has_one :user
+  attributes :id, :title, :description, :location, :start_date, :end_date, :duration, :aasm_state, :user
+  belongs_to :user
+
+  def duration
+    (object.end_date.to_date - object.start_date.to_date).to_i
+  end
+
+  def start_date
+    object.start_date.strftime('%Y-%B-%d')
+  end
+
+  def end_date
+    object.end_date.strftime('%Y-%B-%d')
+  end
+
+  def user
+    object.user.full_name
+  end
+  
 end
