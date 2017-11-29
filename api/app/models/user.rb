c78f264a7fa4ed985fbd433c69ae28c0a18748e4
conorhalloran <conorhalloran@gmail.com>
2017-11-29 09:27:36 -0800

    Setup React. Communicating with Backend. Created TripsIndexPage, TripList and Trip Summary

--- api/app/models/user.rb
+++ api/app/models/user.rb
@@ -11,4 +11,8 @@ class User < ApplicationRecord
 
     has_secure_password
     validates :password, presence: true, length: { minimum: 6 }
+
+    def full_name
+        "#{first_name} #{last_name}".titleize
+    end
 end
