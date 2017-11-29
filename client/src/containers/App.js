69c20ee00bdc21c68951dcbaba49bed36954b377
conorhalloran <conorhalloran@gmail.com>
2017-11-29 10:21:41 -0800

    JWT Integrated with React Client front end. Create SignInForm and SignInPage.

--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -12,38 +12,72 @@ import AuthRoute from '../components/AuthRoute';
 
 // PAGES
 import TripsIndexPage from './TripsIndexPage';
-
+import SignInPage from './SignInPage';
 // APP
 class App extends Component {
-  constructor (props) {
+	constructor (props) {
 		super(props);
 
 		this.state = {
 			user: {}
 		};
+		this.signIn = this.signIn.bind(this);
+		this.signOut = this.signOut.bind(this);
+	}
+	componentDidMount () {
+		this.signIn();
 	}
 
+	//FUNCTIONS
+	signIn () {
+		const jwt = localStorage.getItem('jwt');
+		if (jwt) {
+			const payload = jwtDecode(jwt);
+			this.setState({user: payload});
+		}
+	}
 
-  //FUNCTIONS
-_renderNavBar () {
-  return (
-    <nav>
-			<img src={logo} className="App-logo" alt="logo" />
-      <h1 className="App-title">Biddr</h1>
-      <Link to="/">Home</Link>
-      <Link to="/trips">Trips</Link>
-    </nav>
-  );
-}
+	isSignedIn () {
+		return !!this.state.user.id;
+	}
+
+	signOut (event) {
+		event.preventDefault();
+		localStorage.clear();
+		this.setState({user: {}});
+	}
+
+	_renderNavBar () {
+		return (
+			<nav>
+				<img src={logo} className="App-logo" alt="logo" />
+				<h1 className="App-title">TripBGreat</h1>
+				<Link to="/">Home</Link>
+				<Link to="/trips">Trips</Link>
+				{ this.isSignedIn()
+					? <span className="flex-row">
+                Hello, {this.state.user.full_name}
+                &nbsp;
+						<Link to="/" onClick={this.signOut}>Sign Out</Link>
+					</span>
+					: <Link to="/sign_in">Sign In</Link>
+				}
+			</nav>
+		);
+	}
 
 	render() {
-    return (
+		return (
 			<Router>
 			  <div className="App">
 					{this._renderNavBar()}
 
 					<Switch>
 						<Route path="/trips" component={TripsIndexPage} />
+						<Route
+							path="/sign_in"
+							render={props => <SignInPage {...props} onSignIn={this.signIn} />}
+						/>
 					</Switch>
   
 				</div>
