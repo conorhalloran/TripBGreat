f33a410c2f5401c8299a867e0c9c0d4c08ba1b7d
conorhalloran <conorhalloran@gmail.com>
2017-12-07 16:40:10 -0800

    Final Add for Demo

--- client/src/containers/HomePage.js
+++ client/src/containers/HomePage.js
@@ -65,7 +65,13 @@ class HomePage extends Component {
 							<Col>
 								<div className="content">
 									<h2>Trip Planning Made Ease</h2>
-									<p>TripBGreat was designed with you in mind. </p>
+									<p>
+										TripBGreat was designed with you in mind. Create an Account
+										to Start Creating Trips. You can invite your friends to join
+										you on trips. Each trip be organized by Days and Activites.
+										Gone are the days of frustrating trip coordination. Will
+										TripBGreat, your Good trips will be Great!
+									</p>
 								</div>
 							</Col>
 
